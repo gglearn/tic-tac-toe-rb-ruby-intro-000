a4b454c32b849289b5bade553d9e398a70605a6a
GG Learn <ozolsdeko@gmail.com>
2017-10-31 18:29:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index-1)
+  index.to_i.between?(0,8) && !position_taken?(board, index.to_i-1)
 
   # if index.between?(0,8)
   #  if !position_taken?(board, index)
