a659b35855bdb86e713c331ac437691b0577ab68
GG Learn <ozolsdeko@gmail.com>
2017-10-31 19:10:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = STDIN.gets.strip.to_i
+  input = STDIN.gets.strip
   index = input_to_index(input)
   puts "Board: #{board}"
   puts "Index: #{index}"
