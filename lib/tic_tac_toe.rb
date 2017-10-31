854633846ca5a539388ca834f4e46e87e9b32b89
GG Learn <ozolsdeko@gmail.com>
2017-10-31 18:13:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,62 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [2,4,6], # Diognal from right
+  [0,4,8], # Diognal from left
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8]  # Right column
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+  if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
+      board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      return combo
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all? {|a| a.match(/[XO]/)}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+      board[won?(board).first]
+  end
+end
