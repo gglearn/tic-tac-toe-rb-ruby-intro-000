e879e749f93aa0d1ab9548c3012c999f65077b92
GG Learn <ozolsdeko@gmail.com>
2017-10-31 19:34:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,8 +43,10 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
+  input = gets.strip
+  index = input_to_index(input)
+  # puts "Board: #{board}"
+  # puts "Index: #{index}"
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
@@ -52,7 +54,6 @@ def turn(board)
     turn(board)
   end
 end
-
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
